af81b315449eb06d427bc7aedda387c4a1a84a78
Gregori <gregoryansp@gmail.com>
2022-09-01 22:17:40 +0300

    first test commit

---
+++ script.js
@@ -0,0 +1,36 @@
+"use strict";
+let tovar = {
+  sigars: "filip moris",
+  sigars1: "jti",
+  company: {
+    jti: "LD",
+    FMU: "Parlament",
+    Imperial: "West",
+    one: 1n,
+  },
+};
+tovar.company.MITJ = ["strong", 3];
+console.log(tovar.sigars);
+console.log(tovar.company.FMU);
+console.log(tovar.company.MITJ);
+console.log(typeof tovar.company.one);
+
+const bool = 2;
+const bool1 = "grrgot";
+const bool2 = false;
+const bool3 = -4 / 0;
+console.log(typeof bool);
+console.log(typeof bool1);
+console.log(typeof bool2);
+console.log(bool3);
+
+// const user = confirm("Are you here?");
+// console.log(user);
+const answer = [];
+answer[0] = prompt("как вас зовут");
+answer[1] = prompt("как ваша фамилия");
+answer[2] = prompt("как ваше отчество");
+
+console.log(typeof dfdfdfdf);
+console.log(9 % 3);
+console.log(true - false);
